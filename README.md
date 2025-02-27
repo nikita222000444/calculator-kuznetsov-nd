f35294092e6e65f81e756a3693911b407266d7c6
nikita222000444 <26840361+nikita222000444@users.noreply.github.com>
2025-02-27 19:28:47 +0300

    Minimum done

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
 # calculator-kuznetsov-nd
 A program written in C. It reads an arithmetic expression from standard input, parses it, and prints the result. The program supports the following operators on integers: `+` , `-` , `*` , `/` , `(` , and `)` . Any whitespace characters are allowed in input. 
 
-Program does not provide any prompt to user — just reading input until `EOF`.
- 
-The input is shorter that 1KiB, all numbers are non negative integers fit into `int` type at any stage of evaluation.
- 
-Program outputs only a number. The whole program fits single `main.c` file. No any external dependencies are used except `libc`. 
+## More in depth about the program
+
+### Building the Program
+To build the program, you need a C compiler such as `gcc` or `clang`. Follow these steps:
+1. Open a terminal.
+2. Navigate to the directory containing the source code(`main.c`).
+3. Run the following command to compile the program.
+```
+gcc main.c -o calculator.exe
+```
+This will create an executable file named calculator.exe.
+
+### Running the Program
+To run the program in integer mode, use the following command:
+```
+./calculator.exe
+```
+Use following command to run program in *real* numbers mode
+```
+./calculator.exe --float
+```
+The program will prompt you to enter an arithmetic expression. After entering the expression, press `Enter` to see the result.
+
+### Important remarks
+- Input passed through `stdin`
+- Output passed through `stdout`
+- Less than 1KiB of input data (else `UB`)
+- Only allowed charset `[0-9()*+\/\s\-]` (else code not equal to `0` returns)
+  - `0-9` matches a single character in the range between 0 and 9 
+  - `()*+` matches a single character in the list `()*+`
+  - `\/` matches the character `/` 
+  - `\s` matches any whitespace character (equivalent to `[\r\n\t\f\v ]` )
+  - `\-` matches the character `-`
+- Only correct arithmetic expression are allowed (else code not equal to `0` returns)
+- All numbers in a given expression are integers from a range $[0 \dots 2 \times 10^9]$ (else `UB`)
+- All intermediate results (for any allowed order of evaluation) are fit into range $[-2 \times 10^9 \dots + 2\times 10^9]$ (else `UB`)
+- Supports flag `--float` that switch app calculations into *real* numbers mode
+- In integer mode:
+  - `/` return an integer part of an division (rounds towards `-0`)
+  - Answer must be given on a single line as an integer number (with `-` sign if negative)
+- In *real* numbers mode:
+  - `/` return a fraction number
+  - Answer must be given on a single line as a real number (decimal notation) with an absolute accuracy $\pm 10^{-4}$
+In any mode division on a number less than $10^{-4}$ are forbidden (else `UB`)
---
+++ src/main.c
@@ -0,0 +1,188 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ignore_spaces(char** ptr_) {
+  while (isspace(**ptr_) || **ptr_ == '\r' || **ptr_ == '\n' || **ptr_ == '\t')
+    ++(*ptr_);
+}
+
+int parse_num(char** ptr_) {
+  if (!isdigit(**ptr_)) {
+    printf("Digit expected but met %c\n", **ptr_);
+    exit(1);
+  }
+
+  int res = 0;
+
+  while (isdigit(**ptr_)) {
+    res = res * 10 + (**ptr_ - '0');
+    ++(*ptr_);
+  }
+  return res;
+}
+
+int calc_expression_int(char** ptr_);
+
+int calc_factor_int(char** ptr_) {
+  ignore_spaces(ptr_);
+  if (**ptr_ == '(') {
+    ++(*ptr_);
+    int num = calc_expression_int(ptr_);
+    ignore_spaces(ptr_);
+    if (**ptr_ != ')') {
+      printf("There is no \')\'\n");
+      exit(1);
+    }
+    ++(*ptr_);
+    return num;
+  }
+  return parse_num(ptr_);
+}
+
+int calc_term_int(char** ptr_) {
+  int fac = calc_factor_int(ptr_);
+  int next_fac;
+  ignore_spaces(ptr_);
+  char operation;
+  while (**ptr_ == '*' || **ptr_ == '/') {
+    operation = **ptr_;
+    ++(*ptr_);
+    next_fac = calc_factor_int(ptr_);
+    ignore_spaces(ptr_);
+
+    if (operation == '*')
+      fac *= next_fac;
+    else {
+      if (next_fac != 0)
+        fac /= next_fac;
+      else {
+        printf("Dividing by 0 is prohibited\n");
+        exit(1);
+      }
+    }
+  }
+  return fac;
+}
+
+int calc_expression_int(char** ptr_) {
+  int term = calc_term_int(ptr_);
+  int next_term;
+  ignore_spaces(ptr_);
+  char operation;
+  if (**ptr_ != '+' && **ptr_ != '-' && **ptr_ != '\0') {
+    printf("Operation expected but met %c\n", **ptr_);
+    exit(1);
+  }
+  while (**ptr_ == '+' || **ptr_ == '-') {
+    operation = **ptr_;
+    ++(*ptr_);
+    next_term = calc_term_int(ptr_);
+    ignore_spaces(ptr_);
+
+    if (**ptr_ != '+' && **ptr_ != '-' && **ptr_ != '\0' && **ptr_ != ')') {
+      printf("Operation expected but met %c\n", **ptr_);
+      exit(1);
+    }
+
+    if (operation == '+')
+      term += next_term;
+    else
+      term -= next_term;
+  }
+  return term;
+}
+
+float calc_expression_float(char** ptr_);
+
+float calc_factor_float(char** ptr_) {
+  ignore_spaces(ptr_);
+  if (**ptr_ == '(') {
+    ++(*ptr_);
+    float num = calc_expression_float(ptr_);
+    ignore_spaces(ptr_);
+    if (**ptr_ != ')') {
+      printf("There is no \')\'\n");
+      exit(1);
+    }
+    ++(*ptr_);
+    return num;
+  }
+  return parse_num(ptr_);
+}
+
+float calc_term_float(char** ptr_) {
+  float fac = calc_factor_float(ptr_);
+  float next_fac;
+  ignore_spaces(ptr_);
+  char operation;
+
+  while (**ptr_ == '*' || **ptr_ == '/') {
+    operation = **ptr_;
+    ++(*ptr_);
+
+    next_fac = calc_factor_float(ptr_);
+    ignore_spaces(ptr_);
+
+    if (operation == '*')
+      fac *= next_fac;
+    else {
+      if (next_fac != 0)
+        fac /= next_fac;
+      else {
+        printf("Dividing by 0 is prohibited\n");
+        exit(1);
+      }
+    }
+  }
+  return fac;
+}
+
+float calc_expression_float(char** ptr_) {
+  float term = calc_term_float(ptr_);
+  float next_term;
+  ignore_spaces(ptr_);
+  char operation;
+
+  if (**ptr_ != '+' && **ptr_ != '-' && **ptr_ != '\0') {
+    printf("Operation expected but met %c\n", **ptr_);
+    exit(1);
+  }
+  while (**ptr_ == '+' || **ptr_ == '-') {
+    operation = **ptr_;
+    ++(*ptr_);
+
+    next_term = calc_term_float(ptr_);
+    ignore_spaces(ptr_);
+
+    if (**ptr_ != '+' && **ptr_ != '-' && **ptr_ != '\0' && **ptr_ != ')') {
+      printf("Operation expected but met %c\n", **ptr_);
+      exit(1);
+    }
+
+    if (operation == '+')
+      term += next_term;
+    else
+      term -= next_term;
+  }
+
+  return term;
+}
+#ifndef GTEST
+int main(int argc, char* argv[]) {
+  printf("Enter the expression\n");
+  char* expression = malloc(sizeof(char) * 1025);
+  if (fgets(expression, sizeof(char) * 1025, stdin) == NULL) {
+    printf("Expression is not expression\n");
+    return 1;
+  }
+
+  char** ptr = &expression;
+  if (argc == 1)
+    printf("%d\n", calc_expression_int(ptr));
+  else if (strcmp(argv[1], "--float") == 0)
+    printf("%.4f\n", calc_expression_float(ptr));
+  return 0;
+}
+#endif
